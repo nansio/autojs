06b93f5b7e5a3cf8f3c3f6a2caad101c5c7e1bd4
少壮要努力 <zhu.yn.m@foxmail.com>
2021-10-28 21:52:55 +0800

    update test.js

---
+++ scripts/test.js
@@ -0,0 +1,11 @@
+// launch('com.taobao.taobao')
+// id("rv_main_container").findOne().children().forEach(child => {
+// 	var target = child.findOne(className("android.widget.FrameLayout").desc("双十一喵糖总动员互动游戏"));
+// 	target && target.click();
+// 	className("android.widget.Button").text("赚糖领红包").findOne().click();
+// });
+
+// className("android.widget.Button").text("赚糖领红包").findOne().click();
+
+
+toast(currentActivity())
